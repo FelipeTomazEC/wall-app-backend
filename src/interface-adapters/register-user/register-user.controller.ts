15a6952def02c32c0c294a9a4c9487c0024805ad
FelipeTomazEC <felipetomaz2206@gmail.com>
2022-04-03 12:17:24 -0300

    feat: implement register user http controller

--- src/interface-adapters/register-user/register-user.controller.ts
+++ src/interface-adapters/register-user/register-user.controller.ts
@@ -2,7 +2,8 @@ import { MissingRequiredFieldError } from "@interface-adapters/shared/errors/mis
 import { HttpController } from "@interface-adapters/shared/http-controller";
 import { HttpRequest } from "@interface-adapters/shared/http-request";
 import { RegisterUserRequest } from "@use-cases/register-user/dtos/request";
-import { Either } from "@utils/either";
+import { Either, failure, success } from "@utils/either";
+import { isEmptyString } from "@utils/is-empty-string";
 
 export class RegisterUserController extends HttpController<RegisterUserRequest> {
   protected extractParameters(httpRequest: HttpRequest): RegisterUserRequest {
@@ -10,7 +11,18 @@ export class RegisterUserController extends HttpController<RegisterUserRequest> 
     return { name, email }; 
   }
 
-  protected checkForMissingParams(request: RegisterUserRequest): Either<MissingRequiredFieldError, null> {
-    
+  protected checkForMissingParams(request: RegisterUserRequest): Either<MissingRequiredFieldError, void> {
+    const { name, email } = request;
+    const isNameMissing = isEmptyString(name);
+    if(isNameMissing) {
+      return failure(new MissingRequiredFieldError('name'));
+    }
+
+    const isEmailMissing = isEmptyString(email);
+    if(isEmailMissing) {
+      return failure(new MissingRequiredFieldError('email'));
+    }
+
+    return success(undefined);
   }
 }
