8dafc2164f0edfb6a6e598cf482b0ef4240b25b9
FelipeTomazEC <felipetomaz2206@gmail.com>
2022-04-04 21:21:11 -0300

    feat: create register user presenter using express

---
+++ src/infra/express/presenters/register-user.presenter.ts
@@ -0,0 +1,28 @@
+import { HttpStatusCode } from "@interface-adapters/shared/http-status-code";
+import { UseCaseOutputPort } from "@use-cases/interfaces/use-case-output-port";
+import { RegisterUserResponse as Response } from "@use-cases/register-user/dtos/response";
+import { EmailAlreadyRegisteredError } from "@use-cases/register-user/errors/email-already-registered-error";
+import { InvalidEmailError } from "@use-cases/register-user/errors/invalid-email-error";
+import { Response as ExpressResponse } from 'express';
+
+export class RegisterUserPresenter implements UseCaseOutputPort<Response> {
+  constructor(private readonly response: ExpressResponse) {}
+
+  async failure(error: Error): Promise<void> {
+    const errorMap = new Map<string, HttpStatusCode>([
+      [EmailAlreadyRegisteredError.name, HttpStatusCode.CONFLICT],
+      [InvalidEmailError.name, HttpStatusCode.BAD_REQUEST],
+    ]);
+
+    const statusCode = errorMap.get(error.constructor.name) ?? HttpStatusCode.SERVER_ERROR;
+    this.response.status(statusCode).json({
+      error: {
+        message: error.message
+      }
+    });
+  }
+
+  async success(response: Response): Promise<void> {
+    this.response.status(HttpStatusCode.RESOURCE_CREATED).json(response);
+  }
+}
