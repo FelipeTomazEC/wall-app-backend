e738f4c969f11525100f36ecb6426be13ab3dcef
FelipeTomazEC <felipetomaz2206@gmail.com>
2022-04-06 20:27:11 -0300

    feat: implement  error logger using console

---
+++ src/infra/implementations/console-error-logger.ts
@@ -0,0 +1,9 @@
+import { ErrorLogger } from "@interface-adapters/interfaces/logger";
+
+export class ConsoleErrorLogger implements ErrorLogger {
+  async log(error: Error): Promise<void> {
+    console.log('Error name', error.name);
+    console.log('Error message', error.message);
+    console.log('Error stack trace', error.stack);
+  }
+}
