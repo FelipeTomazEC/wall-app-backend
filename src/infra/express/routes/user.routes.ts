d2e1d784b456ac5c5b6844c7fcdc180160eac99c
FelipeTomazEC <felipetomaz2206@gmail.com>
2022-04-06 20:32:23 -0300

    feat: implement user express router

---
+++ src/infra/express/routes/user.routes.ts
@@ -0,0 +1,12 @@
+import { RequestHandler, Router } from 'express';
+
+export type UserHandlers = {
+  register: RequestHandler;
+}
+
+export const getUserRoutes = (handlers: UserHandlers): Router => {
+  const router = Router();
+  router.post('/users', handlers.register);
+
+  return router;
+}
