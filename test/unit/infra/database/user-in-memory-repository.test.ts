bd01c0a28f046e90fe9d2edef3f0a4fa853000c2
FelipeTomazEC <felipetomaz2206@gmail.com>
2022-04-06 18:36:00 -0300

    test: implement tests for user in memory repo

---
+++ test/unit/infra/database/user-in-memory-repository.test.ts
@@ -0,0 +1,27 @@
+import { User } from "@entities/user";
+import { UserInMemoryRepository } from "@infra/database/user-in-memory-repository";
+import faker from 'faker';
+
+describe('User in memory repository tests', () => {
+  const sut = UserInMemoryRepository.getInstance();
+
+  it('should be singleton', () => {
+    const anotherInstance = UserInMemoryRepository.getInstance();
+    expect(sut === anotherInstance).toBeTruthy();
+  });
+
+  it('should save the user', async () => {
+    const user = new User({
+      email: faker.internet.email(),
+      id: faker.datatype.uuid(),
+      name: faker.name.findName()
+    });
+
+    const existsBeforeSaving = await sut.emailExists(user.email);
+    await sut.save(user);
+    const existsAfterSaving = await sut.emailExists(user.email);
+    
+    expect(existsBeforeSaving).toBeFalsy();
+    expect(existsAfterSaving).toBeTruthy();
+  });
+})
