f6af6033f937d8cb2385473f4d1dd970b898e354
FelipeTomazEC <felipetomaz2206@gmail.com>
2022-04-08 15:42:56 -0300

    feat: create auth handlers factory

---
+++ src/infra/main/make-auth-handlers.ts
@@ -0,0 +1,22 @@
+import { UserInMemoryRepository } from "@infra/database/user-in-memory-repository";
+import { authenticateUserHandler } from "@infra/express/handlers/authenticate-user.handler";
+import { AuthHandlers } from "@infra/express/routes/auth.routes";
+import { BcryptPasswordEncrypter } from "@infra/implementations/bcrypt-password-encrypter";
+import { ConsoleErrorLogger } from "@infra/implementations/console-error-logger";
+import { JWTAuthService } from "@infra/implementations/jwt-auth-service";
+
+export const makeAuthHandlers = (): AuthHandlers => {
+  const repository = UserInMemoryRepository.getInstance();
+  const logger = new ConsoleErrorLogger();
+  const encrypter = new BcryptPasswordEncrypter();
+  const authService = new JWTAuthService();
+
+  const authenticate = authenticateUserHandler({ 
+    repository, 
+    logger, 
+    encrypter, 
+    authService 
+  });
+
+  return { authenticate };
+}
