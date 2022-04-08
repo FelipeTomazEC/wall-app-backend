383fafaa720697c4c49856ce634cce898c66f4a1
FelipeTomazEC <felipetomaz2206@gmail.com>
2022-04-08 11:27:18 -0300

    feat: use the encrypter to verify password

--- src/use-cases/authenticate/index.ts
+++ src/use-cases/authenticate/index.ts
@@ -1,3 +1,4 @@
+import { PasswordEncrypter } from "@use-cases/interfaces/password-encrypter.interface";
 import { UseCaseInputPort } from "@use-cases/interfaces/use-case-input-port";
 import { UseCaseOutputPort } from "@use-cases/interfaces/use-case-output-port";
 import { AuthenticationService } from "./dependencies/authentication-service.interface";
@@ -10,21 +11,22 @@ type Dependencies = {
   presenter: UseCaseOutputPort<Response>;
   repository: GetByEmailRepository;
   authService: AuthenticationService;
+  encrypter: PasswordEncrypter;
 }
 
 export class AuthenticateUseCase implements UseCaseInputPort<Request> {
   constructor(private readonly dependencies: Dependencies) {}
 
   async execute(request: Request): Promise<void> {
-    const { repository, presenter, authService } = this.dependencies;
-
-    const user = await repository.getByEmail(request.email);
+    const { repository, presenter, authService, encrypter } = this.dependencies;
+    const { password, email } = request;
+    const user = await repository.getByEmail(email);
     if(!user) {
       return presenter.failure(new AuthenticationError());
     }
 
-    const isPasswordWrong = user.password !== request.password;
-    if(isPasswordWrong) {
+    const isPasswordCorrect = await encrypter.verify(password, user.password);
+    if(!isPasswordCorrect) {
       return presenter.failure(new AuthenticationError());
     }
 
