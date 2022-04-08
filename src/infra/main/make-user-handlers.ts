2aaaf764c23071e491f0da537ce558c35a6a98e7
FelipeTomazEC <felipetomaz2206@gmail.com>
2022-04-08 10:45:42 -0300

    feat: inject bcrypt encrypter

--- src/infra/main/make-user-handlers.ts
+++ src/infra/main/make-user-handlers.ts
@@ -1,6 +1,7 @@
 import { UserInMemoryRepository } from "@infra/database/user-in-memory-repository";
 import { registerUserHandler } from "@infra/express/handlers/register-user.handlers";
 import { UserHandlers } from "@infra/express/routes/user.routes";
+import { BcryptPasswordEncrypter } from "@infra/implementations/bcrypt-password-encrypter";
 import { ConsoleErrorLogger } from "@infra/implementations/console-error-logger";
 import { EmailValidatorImpl } from "@infra/implementations/email-validator-impl";
 import { NodeMailerEmailSender } from "@infra/implementations/nodemailer-email-sender";
@@ -12,13 +13,15 @@ export const makeUserHandlers = (): UserHandlers => {
   const emailValidator = new EmailValidatorImpl();
   const logger = new ConsoleErrorLogger();
   const idGenerator = new UUIDv4IdGenerator();
+  const encrypter = new BcryptPasswordEncrypter();
 
   const register = registerUserHandler({
     emailSender, 
     emailValidator,
     repository, 
     logger,
-    idGenerator
+    idGenerator,
+    encrypter
   })
 
   return {
