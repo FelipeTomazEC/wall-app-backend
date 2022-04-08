63d8f54a6162ca4c7cb2a6e598f1d40a59f03310
FelipeTomazEC <felipetomaz2206@gmail.com>
2022-04-08 15:45:36 -0300

    feat: add auth routes in application

--- src/infra/main/setup-routes.ts
+++ src/infra/main/setup-routes.ts
@@ -1,7 +1,10 @@
 import { getUserRoutes } from "@infra/express/routes/user.routes"
 import { makeUserHandlers } from "./make-user-handlers"
 import { Express } from 'express';
+import { getAuthRoutes } from "@infra/express/routes/auth.routes";
+import { makeAuthHandlers } from "./make-auth-handlers";
 
 export const setupRoutes = (application: Express) => {
   application.use(getUserRoutes(makeUserHandlers()));
+  application.use(getAuthRoutes(makeAuthHandlers()));
 }
