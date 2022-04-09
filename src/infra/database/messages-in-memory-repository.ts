0a6c1b4fb9f3a11278f7b45d496eaaa590fdf393
FelipeTomazEC <felipetomaz2206@gmail.com>
2022-04-09 16:09:52 -0300

    feat: create messages in memory repository

---
+++ src/infra/database/messages-in-memory-repository.ts
@@ -0,0 +1,24 @@
+import { Message } from "@entities/message";
+import { SaveRepository } from "@use-cases/interfaces/repository";
+
+export class MessagesInMemoryRepository implements SaveRepository<Message> {
+  private static instance: MessagesInMemoryRepository | null = null;
+
+  private readonly messages: Message[];
+
+  private constructor() {
+    this.messages = [];
+  }
+
+  static getInstance(): MessagesInMemoryRepository {
+    if(!this.instance) {
+      this.instance = new MessagesInMemoryRepository();
+    }
+
+    return this.instance;
+  } 
+
+  async save(message: Message): Promise<void> {
+    this.messages.push(message);
+  }
+}
