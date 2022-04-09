26ec9644d1976a089a68411a47be9c2f0daedbd0
FelipeTomazEC <felipetomaz2206@gmail.com>
2022-04-09 16:38:41 -0300

    feat: implement authorization service interface

--- src/infra/implementations/jwt-auth-service.ts
+++ src/infra/implementations/jwt-auth-service.ts
@@ -1,8 +1,15 @@
 import { User } from "@entities/user";
 import { AuthenticationService } from "@use-cases/authenticate/dependencies/authentication-service.interface";
-import { sign, SignOptions } from 'jsonwebtoken';
+import { AuthorizationService } from "@use-cases/post-message/dependencies/authorization-service.interface";
+import { AuthorizationError } from "@use-cases/post-message/errors/authorization-error";
+import { Either, failure, success } from "@utils/either";
+import { sign, SignOptions, verify } from 'jsonwebtoken';
 
-export class JWTAuthService implements AuthenticationService {
+type TokenPayload = {
+  ownerId: string;
+}
+
+export class JWTAuthService implements AuthenticationService, AuthorizationService {
   generateTokenFor(user: User): Promise<string> {
     const secret = process.env.JWT_SECRET!;
     const { JWT_TIME_TO_LIVE_IN_HOURS = '24' } = process.env;
@@ -18,5 +25,21 @@ export class JWTAuthService implements AuthenticationService {
         return resolve(token!);
       });
     });
+  }
+
+  async authorize(token: string): Promise<Either<AuthorizationError, string>> {
+    const secret = process.env.JWT_SECRET!;
+
+    return new Promise((resolve) => {
+      verify(token, secret, (error, payload) => {
+        if(error) {
+          return resolve(failure(new AuthorizationError()));
+        }
+
+        const { ownerId } = payload as TokenPayload;
+
+        return resolve(success(ownerId));
+      })
+    })
   }
 }
