13ae80562a12997c9c402b8737de45d5b563ae07
FelipeTomazEC <felipetomaz2206@gmail.com>
2022-04-09 18:33:24 -0300

    feat: create retrieve messages use case

---
+++ src/use-cases/retrieve-messages/dtos/response.ts
@@ -0,0 +1,9 @@
+export type MessageDTO = {
+  username: string;
+  postedAt: Date;
+  text: string;
+}
+
+export type RetrieveMessagesResponse = {
+  messages: MessageDTO[];
+}
---
+++ src/use-cases/retrieve-messages/index.ts
@@ -0,0 +1,27 @@
+import { Message } from "@entities/message";
+import { GetAllRepository } from "@use-cases/interfaces/repository";
+import { UseCaseInputPort } from "@use-cases/interfaces/use-case-input-port";
+import { UseCaseOutputPort } from "@use-cases/interfaces/use-case-output-port";
+import { RetrieveMessagesResponse } from "./dtos/response";
+
+type Dependencies = {
+  presenter: UseCaseOutputPort<RetrieveMessagesResponse>;
+  messagesRepo: GetAllRepository<Message>;
+}
+
+export class RetrieveMessagesUseCase implements UseCaseInputPort<void> {
+  constructor(private readonly dependencies: Dependencies) {}
+
+  async execute(): Promise<void> {
+    const { messagesRepo, presenter } = this.dependencies;
+    const messages = await messagesRepo.getAll();
+
+    return presenter.success({
+      messages: messages.map(message => ({
+        postedAt: message.postedAt,
+        text: message.text,
+        username: message.username
+      }))
+    });
+  }
+}
