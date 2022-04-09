68823b95df6d6697b1b91fd6aea448d85a8ff26c
FelipeTomazEC <felipetomaz2206@gmail.com>
2022-04-09 17:04:50 -0300

    feat: add message routes in the application

--- src/infra/main/setup-routes.ts
+++ src/infra/main/setup-routes.ts
@@ -3,8 +3,11 @@ import { makeUserHandlers } from "./make-user-handlers"
 import { Express } from 'express';
 import { getAuthRoutes } from "@infra/express/routes/auth.routes";
 import { makeAuthHandlers } from "./make-auth-handlers";
+import { getMessageRoutes } from "@infra/express/routes/message.routes";
+import { makeMessageHandlers } from "./make-message-handlers";
 
 export const setupRoutes = (application: Express) => {
   application.use(getUserRoutes(makeUserHandlers()));
   application.use(getAuthRoutes(makeAuthHandlers()));
+  application.use(getMessageRoutes(makeMessageHandlers()));
 }
