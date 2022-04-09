ea67b516e732d1277034fffa0558accd870a153e
FelipeTomazEC <felipetomaz2206@gmail.com>
2022-04-09 19:04:16 -0300

    feat: create retrieve messages express presenter

---
+++ src/infra/express/presenters/retrieve-messages.presenter.ts
@@ -0,0 +1,20 @@
+import { HttpStatusCode } from "@interface-adapters/shared/http-status-code";
+import { UseCaseOutputPort } from "@use-cases/interfaces/use-case-output-port";
+import { RetrieveMessagesResponse as Response } from "@use-cases/retrieve-messages/dtos/response";
+import { Response as ExpressResponse } from 'express';
+
+export class RetrieveMessagesPresenter implements UseCaseOutputPort<Response> {
+  constructor(private readonly expressResponse: ExpressResponse) {}
+
+  async failure(error: Error): Promise<void> {
+    this.expressResponse.status(HttpStatusCode.SERVER_ERROR).json({
+      error: {
+        message: error.message
+      }
+    });
+  }
+
+  async success(response: Response): Promise<void> {
+    this.expressResponse.status(HttpStatusCode.OK).json(response);
+  }
+}
