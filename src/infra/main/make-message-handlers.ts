caa15483018500366ef9a8df8094d9303921c5ee
FelipeTomazEC <felipetomaz2206@gmail.com>
2022-04-09 16:49:30 -0300

    feat: create message handlers factory

---
+++ src/infra/main/make-message-handlers.ts
@@ -0,0 +1,26 @@
+import { MessageHandlers } from "@infra/express/routes/message.routes";
+import { postMessageHandler } from '@infra/express/handlers/post-message.handler'
+import { JWTAuthService } from "@infra/implementations/jwt-auth-service";
+import { UUIDv4IdGenerator } from "@infra/implementations/uuid-v4-id-generator";
+import { ConsoleErrorLogger } from "@infra/implementations/console-error-logger";
+import { MessagesInMemoryRepository } from "@infra/database/messages-in-memory-repository";
+import { UserInMemoryRepository } from "@infra/database/user-in-memory-repository";
+
+
+export const makeMessageHandlers = (): MessageHandlers => {
+  const authService = new JWTAuthService();
+  const idGenerator = new UUIDv4IdGenerator();
+  const logger = new ConsoleErrorLogger();
+  const messagesRepo = MessagesInMemoryRepository.getInstance();
+  const usersRepo = UserInMemoryRepository.getInstance();
+
+  const postMessage = postMessageHandler({
+    authService,
+    idGenerator,
+    logger,
+    messagesRepo,
+    usersRepo
+  });
+
+  return { postMessage };
+}
