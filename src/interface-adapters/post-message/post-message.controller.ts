752ea1eff961aedd0936d6bd29e0d2ace7487278
FelipeTomazEC <felipetomaz2206@gmail.com>
2022-04-09 15:10:35 -0300

    feat: create post message http controller

---
+++ src/interface-adapters/post-message/post-message.controller.ts
@@ -0,0 +1,23 @@
+import { MissingRequiredFieldError } from "@interface-adapters/shared/errors/missing-required-field-error";
+import { HttpController } from "@interface-adapters/shared/http-controller";
+import { HttpRequest } from "@interface-adapters/shared/http-request";
+import { PostMessageRequest } from "@use-cases/post-message/dtos/request";
+import { Either, failure, success } from "@utils/either";
+import { isEmptyString } from "@utils/is-empty-string";
+
+export class PostMessageController extends HttpController<PostMessageRequest> {
+  protected extractParameters(httpRequest: HttpRequest): PostMessageRequest {
+    const authHeader = httpRequest.getHeader<string>('Authorization') ?? '';
+    const token = authHeader.replace('Bearer ', '');
+    const text = httpRequest.body.message;
+    return { token, text };
+  }
+
+  protected checkForMissingParams(request: PostMessageRequest): Either<MissingRequiredFieldError, void> {
+    if(isEmptyString(request.text)) {
+      return failure(new MissingRequiredFieldError('message'));
+    }
+
+    return success(undefined);
+  }
+}
