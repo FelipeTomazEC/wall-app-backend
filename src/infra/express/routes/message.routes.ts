5e8333e4985914d81ef651dd59955afdee04a2cf
FelipeTomazEC <felipetomaz2206@gmail.com>
2022-04-10 00:33:54 -0300

    feat: create endpoint to retrieve all messages

--- src/infra/express/routes/message.routes.ts
+++ src/infra/express/routes/message.routes.ts
@@ -2,11 +2,13 @@ import { RequestHandler, Router } from "express"
 
 export type MessageHandlers = {
   postMessage: RequestHandler;
+  getAll: RequestHandler;
 }
 
 export const getMessageRoutes = (handlers: MessageHandlers): Router => {
   const router = Router();
   router.post('/messages', handlers.postMessage);
+  router.get('/messages', handlers.getAll);
 
   return router;
 }
