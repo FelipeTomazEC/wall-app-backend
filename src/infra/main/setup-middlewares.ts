fc43d94c96d5b3de6df77aef2e8a5130ce61c8a9
FelipeTomazEC <felipetomaz2206@gmail.com>
2022-04-23 12:00:08 -0300

    chore: config cors

--- src/infra/main/setup-middlewares.ts
+++ src/infra/main/setup-middlewares.ts
@@ -2,6 +2,8 @@ import { Express, json } from "express";
 import cors from 'cors';
 
 export const setupMiddlewares = (application: Express) => {
-  application.use(cors());
+  application.use(cors({
+    origin: process.env.FRONT_END_ORIGIN
+  }));
   application.use(json());
 }
