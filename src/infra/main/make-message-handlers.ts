36ae450c0c9cdf10cd116a43a85d794f00c7c9f1
FelipeTomazEC <felipetomaz2206@gmail.com>
2022-04-24 14:54:27 -0300

    refactor: change message repository implementation

--- src/infra/main/make-message-handlers.ts
+++ src/infra/main/make-message-handlers.ts
@@ -3,18 +3,19 @@ import { postMessageHandler } from '@infra/express/handlers/post-message.handler
 import { JWTAuthService } from "@infra/implementations/jwt-auth-service";
 import { UUIDv4IdGenerator } from "@infra/implementations/uuid-v4-id-generator";
 import { ConsoleErrorLogger } from "@infra/implementations/console-error-logger";
-import { MessagesInMemoryRepository } from "@infra/database/in-memory/messages-in-memory-repository";
 import { retrieveMessagesHandler } from "@infra/express/handlers/retrieve-messages.handler";
 import { PrismaUserRepository } from "@infra/database/prisma/repositories/prisma-user-repository";
 import { PrismaClient } from "@prisma/client";
+import { PrismaMessageRepository } from "@infra/database/prisma/repositories/prisma-message-repository";
 
 
 export const makeMessageHandlers = (): MessageHandlers => {
+  const client = new PrismaClient();
   const authService = new JWTAuthService();
   const idGenerator = new UUIDv4IdGenerator();
   const logger = new ConsoleErrorLogger();
-  const messagesRepo = MessagesInMemoryRepository.getInstance();
-  const usersRepo = new PrismaUserRepository(new PrismaClient());
+  const messagesRepo = new PrismaMessageRepository(client);
+  const usersRepo = new PrismaUserRepository(client);
 
   const postMessage = postMessageHandler({
     authService,
