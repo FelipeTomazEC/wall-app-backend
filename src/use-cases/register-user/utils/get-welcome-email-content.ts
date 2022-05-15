901e7c15deebb07257d67068dbc62a8bac8beb81
FelipeTomazEC <felipetomaz2206@gmail.com>
2022-05-15 20:17:36 -0300

    chore: improve email message

--- src/use-cases/register-user/utils/get-welcome-email-content.ts
+++ src/use-cases/register-user/utils/get-welcome-email-content.ts
@@ -1,7 +1,11 @@
 export const getWelcomeEmailContent = (name: string) => `
   <html>
     <body>
-      <h1> Welcome <strong>${name}</strong>! </h1>
+    <h1><span style="color: #000000;">Welcome ${name}!</span></h1>
+    <p>We are glad to say that now you're also a member of this great community called <strong>The WALL</strong>.</p>
+    <p>Feel free to post your awesome and smart thoughts on our wall.</p>
+    <p>See ya,&nbsp;</p>
+    <p><strong>🧱 The Wall Community</strong></p>
     </body>
   </html>
 `;
